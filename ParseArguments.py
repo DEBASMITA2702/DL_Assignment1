54392453756d2904f96719089099da8663d1161b
DEBASMITA2702 <69621397+DEBASMITA2702@users.noreply.github.com>
2025-03-09 17:45:42 +0530

    Add files via upload

---
+++ ParseArguments.py
@@ -0,0 +1,29 @@
+import warnings
+import argparse
+
+warnings.filterwarnings("ignore")
+
+def collectArguments():
+    argument_passed = argparse.ArgumentParser(description = 'Arguments for models hyperparameters')
+    argument_passed.add_argument('-wp','--wandb_project', help = "Project name used to track experiments in Weights & Biases dashboard")
+    argument_passed.add_argument('-we','--wandb_entity', help = "Wandb Entity used to track experiments in the Weights & Biases dashboard")
+    argument_passed.add_argument('-d','--dataset', help = "Dataset to work on \n choices: ['mnist', 'fashion_mnist']")
+    argument_passed.add_argument('-e','--epochs', type = int, help = "Number of epochs to train neural network")
+    argument_passed.add_argument('-b','--batch_size', type = int, help = "Batch size used to train neural network")
+    argument_passed.add_argument('-l','--loss', help = "Loss function to use for backpropagation \n choices: ['mean_squared_error', 'cross_entropy']")
+    argument_passed.add_argument('-o','--optimizer', help = "Optimization algorithm to use for training the model \n choices: ['stochastic', 'momentum', 'nesterov', 'rmsprop', 'adam', 'nadam']")
+    argument_passed.add_argument('-lr','--learning_rate', type = float, help = "Learning rate used to optimize model parameters")
+    argument_passed.add_argument('-m','--momentum', type = float, help = "Momentum used by momentum and nag optimizers")
+    argument_passed.add_argument('-beta','--beta', type = float, help = "Beta used by rmsprop optimizer")
+    argument_passed.add_argument('-beta1','--beta1', type = float, help = "Beta1 used by adam and nadam optimizers")
+    argument_passed.add_argument('-beta2','--beta2', type = float, help = "Beta2 used by adam and nadam optimizers")
+    argument_passed.add_argument('-eps','--epsilon', type = float, help = "Epsilon used by optimizers")
+    argument_passed.add_argument('-w_d','--weight_decay', type = float, help = "Weight decay used by optimizers")
+    argument_passed.add_argument('-w_i','--weight_init', help = "Weight initialization technique to use \n choices: ['random', 'xavier_nor', 'xavier_uni', 'he_nor', 'he_uni']")
+    argument_passed.add_argument('-nhl','--num_layers', type = int, help = "Number of hidden layers used in feedforward neural network")
+    argument_passed.add_argument('-sz','--hidden_size', type = int, help = "Number of hidden neurons in a feedforward layer")
+    argument_passed.add_argument('-a','--activation', help = "Activation function to use for the model \n choices: ['identity', 'sigmoid', 'tanh', 'relu']")
+    argument_passed.add_argument('-c','--confusion', type = int, help = "Whether you want to generate confusion matrix or not \n choices: [0,1]")
+    argument_passed.add_argument('-t','--test', type = int, help = "Whether you want to calculate and print test accuracy or not \n choices: [0,1]")
+
+    return argument_passed.parse_args()
---
+++ SetHyperparamValues.py
@@ -0,0 +1,92 @@
+from Utilities import checkIfNone
+
+def setValuesFromArgument(argument_passed):
+
+    '''default values of each of the hyperparameter according to the config of my best model obtained from sweeping'''
+    set_project_name = "Debasmita-DA6410-Assignment-1"
+    set_entity_name = "cs24m015-indian-institute-of-technology-madras"
+    set_learning_rate = 0.005
+    set_momentum = 0.999
+    set_beta = 0.999
+    set_beta1 = 0.9
+    set_beta2 = 0.999
+    set_epsilon = 1e-5
+    set_optimizer = "adam"
+    set_batch_size = 128
+    set_loss_type = "cross_entropy"
+    set_epochs = 10
+    set_weight_decay = 0.0005
+    set_dataset_name = "fashion_mnist"
+    set_weight_initialization = "he_nor"
+    set_hidden_layers = 3
+    set_neurons_in_hidden = 128
+    set_activation = "relu"
+    set_confusion_matrix = 0
+    set_test = 0
+
+    '''set the values based on the arguments passed'''
+    if checkIfNone(argument_passed.wandb_project):
+        set_project_name = argument_passed.wandb_project
+    if checkIfNone(argument_passed.wandb_entity):
+        set_entity_name = argument_passed.wandb_entity
+    if checkIfNone(argument_passed.dataset):
+        set_dataset_name = argument_passed.dataset
+    if checkIfNone(argument_passed.learning_rate):
+        set_learning_rate = argument_passed.learning_rate
+    if checkIfNone(argument_passed.momentum):
+        set_momentum = argument_passed.momentum
+    if checkIfNone(argument_passed.beta):
+        set_beta = argument_passed.beta
+    if checkIfNone(argument_passed.beta1):
+        set_beta1 = argument_passed.beta1
+    if checkIfNone(argument_passed.beta2):
+        set_beta2 = argument_passed.beta2
+    if checkIfNone(argument_passed.epsilon):
+        set_epsilon = argument_passed.epsilon
+    if checkIfNone(argument_passed.optimizer):
+        set_optimizer = argument_passed.optimizer
+    if checkIfNone(argument_passed.batch_size):
+        set_batch_size = argument_passed.batch_size
+    if checkIfNone(argument_passed.loss):
+        set_loss_type = argument_passed.loss
+    if checkIfNone(argument_passed.epochs):
+        set_epochs = argument_passed.epochs
+    if checkIfNone(argument_passed.weight_decay):
+        set_weight_decay = argument_passed.weight_decay
+    if checkIfNone(argument_passed.weight_init):
+        set_weight_initialization = argument_passed.weight_init
+    if checkIfNone(argument_passed.num_layers):
+        set_hidden_layers = argument_passed.num_layers
+    if checkIfNone(argument_passed.hidden_size):
+        set_neurons_in_hidden = argument_passed.hidden_size
+    if checkIfNone(argument_passed.activation):
+        set_activation = argument_passed.activation
+    if checkIfNone(argument_passed.confusion):
+        set_confusion_matrix = argument_passed.confusion
+    if checkIfNone(argument_passed.test):
+        set_test = argument_passed.test
+
+    hyperparam_dictionary = dict()
+    
+    hyperparam_dictionary["project_name"] = set_project_name
+    hyperparam_dictionary["entity_name"] = set_entity_name
+    hyperparam_dictionary["learning_rate"] = set_learning_rate
+    hyperparam_dictionary["momentum"] = set_momentum
+    hyperparam_dictionary["beta"] = set_beta
+    hyperparam_dictionary["beta1"] = set_beta1
+    hyperparam_dictionary["beta2"] = set_beta2
+    hyperparam_dictionary["epsilon"] = set_epsilon
+    hyperparam_dictionary["optimizer"] = set_optimizer
+    hyperparam_dictionary["batch_size"] = set_batch_size
+    hyperparam_dictionary["loss_type"] = set_loss_type
+    hyperparam_dictionary["epochs"] = set_epochs
+    hyperparam_dictionary["weight_decay"] = set_weight_decay
+    hyperparam_dictionary["dataset_name"] = set_dataset_name
+    hyperparam_dictionary["weight_initialization"] = set_weight_initialization
+    hyperparam_dictionary["hidden_layers"] = set_hidden_layers
+    hyperparam_dictionary["neurons_in_hidden"] = set_neurons_in_hidden
+    hyperparam_dictionary["activation"] = set_activation
+    hyperparam_dictionary["confusion_matrix"] = set_confusion_matrix
+    hyperparam_dictionary["test"] = set_test
+
+    return hyperparam_dictionary
