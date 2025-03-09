76079721edc1da4e27ef94847b6e4ea47741fed7
DEBASMITA2702 <69621397+DEBASMITA2702@users.noreply.github.com>
2025-03-09 16:18:13 +0530

    Add files via upload

--- Accuracy.py
+++ Accuracy.py
@@ -15,24 +15,3 @@ def trainingAccuracy(true_output, predicted_output):
     accuracy_ratio = accurate_predictions / data_size
     
     return accuracy_ratio
-
-
-def validationAccuracy(weights, biases, input, num_total_layers, activation_function, true_output):
-    accurate_predictions = 0
-    
-    '''doing forward propagation to find out the predictions of the input according to the weight and bias'''
-    _, _, predicted_output = forwardPropagation(weights, biases, input, num_total_layers, activation_function)
-
-    data_size = true_output.shape[0]
-
-    for point in range(data_size):
-        '''checking if the true class value and the predicted class are same or not'''
-        true_class = np.argmax(true_output[point])
-        predicted_class = np.argmax(predicted_output[point])
-
-        if true_class == predicted_class:
-            accurate_predictions+=1
-
-    accuracy_ratio = accurate_predictions / data_size
-
-    return accuracy_ratio
