2cf3a58e2ff6ed6bca3fc19070d9d2adac85f9ee
DEBASMITA2702 <69621397+DEBASMITA2702@users.noreply.github.com>
2025-03-10 10:47:27 +0530

    Add files via upload

--- Question_1.py
+++ Question_1.py
@@ -4,34 +4,36 @@ from matplotlib import pyplot as plt
 from keras.datasets import fashion_mnist
 from LoadDataset import loadAndSplitDataset
 
-'''login to wandb to generate plot'''
+wandb.login()
 
 '''loads dataset from fashion_mnist'''
 x_train, y_train, _, _, _, _ = loadAndSplitDataset(fashion_mnist)
 
 '''gives labels of the 10 output classes'''
-output_class = ["T-shirt/top", "Trouser", "Pullover", "Dress", "Coat", "Sandal", "Shirt", "Sneaker", "Bag", "Ankle boot"]
+output_classes = ["T-shirt/top", "Trouser", "Pullover", "Dress", "Coat", "Sandal", "Shirt", "Sneaker", "Bag", "Ankle boot"]
 
-'''initialize the project'''
+'''creating subplots (2 rows, 5 columns) '''
+fig, axes = plt.subplots(2, 5, figsize=(20, 6))
+axes = axes.flatten()
 
-_, getPlot = plt.subplots(2, 5, figsize = (20, 6))
-getPlot = getPlot.flatten()
-output_images = list()
+'''prepare a list to store the images'''
+wandb_images = []
 
+'''loop over each of the 10 classes'''
 for current_class in range(10):
-  '''get the index where the image of the particular class is present'''
-  image_class = np.argmax(y_train == current_class)
+    '''get the first index in y_train that matches the specific current class'''
+    temp_array = (y_train == current_class)
+    first_match_index = np.argmax(temp_array)
+    
+    '''display the image in the subplot'''
+    axes[current_class].imshow(x_train[first_match_index], cmap="gray")
+    axes[current_class].set_title(output_classes[current_class])
+    
+    '''create a wandb image object'''
+    single_image = wandb.Image(x_train[first_match_index], caption=[output_classes[current_class]])
+    wandb_images.append(single_image)
 
-  '''get the image pixels'''
-  getPlot[current_class].imshow(x_train[image_class],cmap = "gray")
-  getPlot[current_class].set_title(output_class[current_class])
-
-  '''generate plot for each class'''
-  image = wandb.Image(x_train[image_class], caption = [output_class[current_class]])
-  output_images.append(image)
-
-'''log the images into wandb'''
-wandb.login()
-wandb.init(project = "Debasmita-DA6410-Assignment-1", name = "Image for Question 1")
-wandb.log({"Question 1" : output_images})
+'''log all the images into wandb'''
+wandb.init(project="Debasmita-DA6410-Assignment-1", name="Image for Question 1")
+wandb.log({"Question 1": wandb_images})
 wandb.finish()
