f69879267080cca2fb3f292be234395622d108cd
DEBASMITA2702 <69621397+DEBASMITA2702@users.noreply.github.com>
2025-03-17 19:43:54 +0530

    Added Comments

--- Question_10.py
+++ Question_10.py
@@ -7,31 +7,33 @@ from Accuracy import trainingAccuracy
 from keras.datasets import mnist
 
 if __name__ == '__main__':
-    '''loads the dataset'''
+    
+    # loads the dataset and splits it to training, validation and test sets
     x_train, y_train, x_val, y_val, x_test, y_test = loadAndSplitDataset(mnist)
 
-    '''flatten input and one hot encode output'''
+    # flatten the input and one hot encode the output
     x_test_flattened = flattenAndNormalizeInputImage(x_test)
     y_test_one_hot = oneHotEncodeOutput(y_test)
 
 
-    '''
-        1st model
-        This is the best model obtained from sweeping
-        The configuration is :
-            activation function : relu
-            batch size : 128
-            beta value : 0.999
-            initialization method : he normal
-            learning rate : 0.005
-            neurons in each hidden layer : 128
-            number of epochs : 10
-            number of hidden layers : 3
-            optimization alogrithm : adam
-            loss function = cross_entropy
-            weight decay : 0.0005
-    '''
+    # 1st model
+    # This is the best model obtained from sweeping
+    # The configuration is :
+    #     activation function : relu
+    #     batch size : 128
+    #     beta value : 0.999
+    #     initialization method : he normal
+    #     learning rate : 0.005
+    #     neurons in each hidden layer : 128
+    #     number of epochs : 10
+    #     number of hidden layers : 3
+    #     optimization alogrithm : adam
+    #     loss function = cross_entropy
+    #     weight decay : 0.0005
+
     print("\n\n1st Model")
+
+    # create the object of the model and call the fitting function
     First_model = NeuralNetwork(x_train, y_train, x_val, y_val, epochs = 10, num_hidden_layers = 3, neurons_in_hidden_layer = 128, 
                                   initialization_method = "he_nor", activation_function = "relu", loss_function = "cross_entropy", 
                                   learning_rate = 0.005, weight_decay = 0.0005)
@@ -39,7 +41,7 @@ if __name__ == '__main__':
     learned_weights, learned_biases = First_model.fitModel(beta = 0.999, beta1 = 0.9, beta2 = 0.999, epsilon = 1e-5, optimizer = "adam", 
                            batch_size = 128, mnistTestRun = True)
     
-    '''get the predictions of the test images based on the weights and biases learned'''
+    # get the predictions of the test images by running forward propagation with the weights and biases learned
     predictions = list()
     data_size = y_test_one_hot.shape[0]
     for point in range(data_size):
@@ -48,87 +50,85 @@ if __name__ == '__main__':
                                                                  First_model.activation_function)
         predictions.append(predictedValues)
     
+    # calculate and print the accuracy
     test_accuracy = trainingAccuracy(y_test_one_hot, np.array(predictions))
-
-    '''print the test accuracy'''
     print("Test Accuracy for Mnist dataset is : ", test_accuracy)
 
 
 
-    '''
-    2nd model
-    This is the second best model obtained from sweeping
-    The configuration is :
-        activation function : relu
-        batch size : 256
-        beta value : 0.999
-        initialization method : he uniform
-        learning rate : 0.005
-        neurons in each hidden layer : 128
-        number of epochs : 20
-        number of hidden layers : 3
-        optimization alogrithm : nadam
-        loss function = cross_entropy
-        weight decay : 0.0005
-    '''
+    # 2nd model
+    # This is the second best model obtained from sweeping
+    # The configuration is :
+    #     activation function : relu
+    #     batch size : 256
+    #     beta value : 0.999
+    #     initialization method : he uniform
+    #     learning rate : 0.005
+    #     neurons in each hidden layer : 128
+    #     number of epochs : 20
+    #     number of hidden layers : 3
+    #     optimization alogrithm : nadam
+    #     loss function = cross_entropy
+    #     weight decay : 0.0005
+
     print("\n\n2nd Model")
-    First_model = NeuralNetwork(x_train, y_train, x_val, y_val, epochs = 20, num_hidden_layers = 3, neurons_in_hidden_layer = 128, 
+
+    # create the object of the model and call the fitting function
+    Second_model = NeuralNetwork(x_train, y_train, x_val, y_val, epochs = 20, num_hidden_layers = 3, neurons_in_hidden_layer = 128, 
                                 initialization_method = "he_uni", activation_function = "relu", loss_function = "cross_entropy", 
                                 learning_rate = 0.005, weight_decay = 0.0005)
 
-    learned_weights, learned_biases = First_model.fitModel(beta = 0.999, beta1 = 0.9, beta2 = 0.999, epsilon = 1e-5, optimizer = "nadam", 
+    learned_weights, learned_biases = Second_model.fitModel(beta = 0.999, beta1 = 0.9, beta2 = 0.999, epsilon = 1e-5, optimizer = "nadam", 
                         batch_size = 256, mnistTestRun = True)
     
-    '''get the predictions of the test images based on the weights and biases learned'''
+    # get the predictions of the test images by running forward propagation with the weights and biases learned
     predictions=list()
     data_size = y_test_one_hot.shape[0]
     for point in range(data_size):
         _, _, predictedValues = forwardPropagationForPredictions(learned_weights, learned_biases, x_test_flattened[point], 
-                                                                First_model.num_total_layers, 
-                                                                First_model.activation_function)
+                                                                Second_model.num_total_layers, 
+                                                                Second_model.activation_function)
         predictions.append(predictedValues)
     
+    # calculate and print the accuracy
     test_accuracy = trainingAccuracy(y_test_one_hot, np.array(predictions))
-
-    '''print the test accuracy'''
     print("Test Accuracy for Mnist dataset is : ", test_accuracy)
 
 
 
-    '''
-    3rd model
-    This is the third best model obtained from sweeping
-    The configuration is :
-        activation function : relu
-        batch size : 256
-        beta value : 0.999
-        initialization method : he normal
-        learning rate : 0.005
-        neurons in each hidden layer : 32
-        number of epochs : 20
-        number of hidden layers : 5
-        optimization alogrithm : nadam
-        loss function = cross_entropy
-        weight decay : 0
-    '''
+    # 3rd model
+    # The configuration is :
+    #     activation function : relu
+    #     batch size : 256
+    #     beta value : 0.999
+    #     initialization method : he normal
+    #     learning rate : 0.005
+    #     neurons in each hidden layer : 32
+    #     number of epochs : 20
+    #     number of hidden layers : 5
+    #     optimization alogrithm : nadam
+    #     loss function = cross_entropy
+    #     weight decay : 0
+    
     print("\n\n3rd Model")
-    First_model = NeuralNetwork(x_train, y_train, x_val, y_val, epochs = 20, num_hidden_layers = 5, neurons_in_hidden_layer = 32, 
+
+    # create the object of the model and call the fitting function
+    Third_model = NeuralNetwork(x_train, y_train, x_val, y_val, epochs = 20, num_hidden_layers = 5, neurons_in_hidden_layer = 32, 
                                 initialization_method = "he_nor", activation_function = "relu", loss_function = "cross_entropy", 
                                 learning_rate = 0.005, weight_decay = 0)
 
-    learned_weights, learned_biases = First_model.fitModel(beta = 0.999, beta1 = 0.9, beta2 = 0.999, epsilon = 1e-5, optimizer = "nadam", 
+    learned_weights, learned_biases = Third_model.fitModel(beta = 0.999, beta1 = 0.9, beta2 = 0.999, epsilon = 1e-5, optimizer = "nadam", 
                         batch_size = 256, mnistTestRun = True)
     
-    '''get the predictions of the test images based on the weights and biases learned'''
+    # get the predictions of the test images by running forward propagation with the weights and biases learned
     predictions=list()
     data_size = y_test_one_hot.shape[0]
     for point in range(data_size):
         _, _, predictedValues = forwardPropagationForPredictions(learned_weights, learned_biases, x_test_flattened[point], 
-                                                                First_model.num_total_layers, 
-                                                                First_model.activation_function)
+                                                                Third_model.num_total_layers, 
+                                                                Third_model.activation_function)
         predictions.append(predictedValues)
     
+    # calculate and print the accuracy
     test_accuracy = trainingAccuracy(y_test_one_hot, np.array(predictions))
-
-    '''print the test accuracy'''
     print("Test Accuracy for Mnist dataset is : ", test_accuracy)
--- Question_8.py
+++ Question_8.py
@@ -3,21 +3,28 @@ from LoadDataset import loadAndSplitDataset
 from FeedForwardNeuralNetwork import NeuralNetwork
 from keras.datasets import fashion_mnist
 
-'''login to wandb to generate plot'''
+'''login to wandb'''
 wandb.login()
 
 def compareLossTypes():
-    '''initialize to project adn create a config'''
+    '''
+    Parameters:
+        none
+    Returns:
+        none
+    Function:
+        runs a sweep on wandb for showing the comparison of cross entropy and mean squared error loss functions
+    '''
+
+    # initialize the project
     wandb.init(project = "Debasmita-DA6410-Assignment-1")
     config = wandb.config
    
-    '''loads the dataset'''
+    # loads the dataset and splits it to training, validation and test sets
     x_train, y_train, x_val, y_val, _, _ = loadAndSplitDataset(fashion_mnist)
     
-    '''create an object of the FeedForwardNeuralNetwork class which has all the required functions
-      pass the parameters to the constructor as a sweep value. this will change the values with each run of the sweep.
-      call the fitting fucntion and pass the optimizer as a parameter
-    '''
+    # create an object of the FeedForwardNeuralNetwork class which has all the required functions
+    # pass the parameters to the constructor as a sweep value. this will change the values with each run of the sweep.
     Model_for_loss_testing = NeuralNetwork(x_train, y_train, x_val, y_val, epochs = config.number_of_epochs, 
                                   num_hidden_layers = config.number_of_hidden_layers, 
                                   neurons_in_hidden_layer = config.neurons_in_each_hidden_layers, 
@@ -27,19 +34,27 @@ def compareLossTypes():
                                   learning_rate = config.learning_rate, 
                                   weight_decay = config.weight_decay)
 
+    # call the fitting fucntion and pass the beta value, optimizer and batch size as parameters
     Model_for_loss_testing.fitModel(beta = config.beta_value, beta1 = 0.9, beta2 = 0.999, epsilon = 1e-5, 
                                     optimizer = config.optimizer_technique, 
                                     batch_size = config.batch_size)
 
 
-'''sweep configuration'''
+'''
+    give the values for the configuration of the different hyperparameters
+    here all the values are same as the best configuration from the sweep of question 4
+    only the loss is given two choices
+'''
 configuration_values = {
+    # the search method is bayes
     'method': 'bayes',
     'name': 'Cross Entropy vs Mean Squared',
+    # search metric is to maximize the validation accuracy
     'metric': {
         'goal': 'maximize',
         'name': 'validation_accuracy'
     },
+    # provide the values for the different hyperparameters
     'parameters': {
         'initialization_method': {'values': ['he_nor']},
         'number_of_hidden_layers' : {'values' : [3]},
@@ -62,4 +77,6 @@ sweep_agent_id = wandb.sweep(sweep = configuration_values, project = "Debasmita-
 
 '''generate a sweep agent to run the sweep'''
 wandb.agent(sweep_agent_id, function = compareLossTypes, count = 2)
+
+'''finish the sweep'''
 wandb.finish()
