318e7b249375fa1d6bfc175889cadd7d4c26f108
DEBASMITA2702 <69621397+DEBASMITA2702@users.noreply.github.com>
2025-03-17 19:30:06 +0530

    Added Comments

--- WandbLog.py
+++ WandbLog.py
@@ -1,6 +1,19 @@
 import wandb
 
 def printAndLogAccuracyAndLoss(epoch, trainingAccuracy, trainingLoss, validationAccuracy, validationLoss):
+    '''
+      Parameters:
+        epoch : curretn epoch number
+        trainingAccuracy : training accuracy calculated
+        trainingLoss : training loss calculated
+        validationAccuracy : validation accuracy calculated
+        validationLoss : validation loss calculated
+      Returns:
+        none
+      Function:
+        prints the accuracy and loss to terminal and logs the same to wandb
+    '''
+
     print("\n")
     print("Epoch = {}".format(epoch + 1))
     print("Training Accuracy = {}".format(trainingAccuracy))
@@ -11,5 +24,15 @@ def printAndLogAccuracyAndLoss(epoch, trainingAccuracy, trainingLoss, validation
 
 
 def printAccuracyForMnistTest(trainingAccuracy, validationAccuracy):
+    '''
+      Parameters:
+        trainingAccuracy : training accuracy calculated
+        validationAccuracy : validation accuracy calculated
+      Returns:
+        none
+      Function:
+        prints the accuracy to terminal
+    '''
+
     print("\nTraining Accuracy for Mnist dataset is : ", trainingAccuracy)
     print("Validation Accuracy for Mnist dataset is : ", validationAccuracy)
--- WeightsAndBiasesAndGradientsInitialization.py
+++ WeightsAndBiasesAndGradientsInitialization.py
@@ -2,56 +2,70 @@ import Utilities
 
 '''function to initialize the weights'''
 def initializeWeight(initialization_method, number_total_layers, neurons_in_hidden_layer, length_input_data, length_output_data):
+    '''
+      Parameters:
+        initialization_method : initailization method for the model
+        number_total_layers : total number of layers for the model
+        neurons_in_hidden_layer : number of neurons in the hidden layer of the model
+        length_input_data : length of input data
+        length_output_data : length of output data
+      Returns:
+        weights : a dictionary containing the weights across all the layers
+      Function:
+        initializes the weights according to the initlization method
+    '''
+
+    # a dictionary to represent the weights
     weights = {}
 
     # weight initialization for random initialization method
     if initialization_method == "random":
-      '''input layer (dimension of weight matrix : hidden x input)'''
+      # input layer (dimension of weight matrix : hidden x input)
       weights[1] = Utilities.random2D(neurons_in_hidden_layer, length_input_data)
-      '''hidden layers (dimension of weight matrix : hidden x hidden)'''
+      # hidden layers (dimension of weight matrix : hidden x hidden)
       for i in range(2, number_total_layers - 1):
         weights[i] = Utilities.random2DFloat(neurons_in_hidden_layer, neurons_in_hidden_layer)
-      '''output layer (dimension of weight matrix : output x hidden)'''
+      # output layer (dimension of weight matrix : output x hidden)
       weights[number_total_layers - 1] = Utilities.random2DFloat(length_output_data, neurons_in_hidden_layer)
 
     # weight initialization for xavier normal initialization method
     elif initialization_method == "xavier_nor":
-      '''input layer (dimension of weight matrix : hidden x input)'''
+      # input layer (dimension of weight matrix : hidden x input)
       weights[1] = Utilities.random2D(neurons_in_hidden_layer, length_input_data) * Utilities.xav_nor(length_input_data, neurons_in_hidden_layer)
-      '''hidden layers (dimension of weight matrix : hidden x hidden)'''
+      # hidden layers (dimension of weight matrix : hidden x hidden)
       for i in range(2, number_total_layers - 1):
         weights[i] = Utilities.random2D(neurons_in_hidden_layer, neurons_in_hidden_layer) * Utilities.xav_nor(neurons_in_hidden_layer, neurons_in_hidden_layer)
-      '''output layer (dimension of weight matrix : output x hidden)'''
+      # output layer (dimension of weight matrix : output x hidden)
       weights[number_total_layers - 1] = Utilities.random2D(length_output_data, neurons_in_hidden_layer) * Utilities.xav_nor(length_output_data, neurons_in_hidden_layer)
 
     # weight initialization for xavier uniform initialization method
     elif initialization_method == "xavier_uni":
-      '''input layer (dimension of weight matrix : hidden x input)'''
+      # input layer (dimension of weight matrix : hidden x input)
       weights[1] = Utilities.random2D(neurons_in_hidden_layer, length_input_data) * Utilities.xav_uni(length_input_data, neurons_in_hidden_layer)
-      '''hidden layers (dimension of weight matrix : hidden x hidden)'''
+      # hidden layers (dimension of weight matrix : hidden x hidden)
       for i in range(2, number_total_layers - 1):
         weights[i] = Utilities.random2D(neurons_in_hidden_layer, neurons_in_hidden_layer) * Utilities.xav_uni(neurons_in_hidden_layer, neurons_in_hidden_layer)
-      '''output layer (dimension of weight matrix : output x hidden)'''
+      # output layer (dimension of weight matrix : output x hidden)
       weights[number_total_layers - 1] = Utilities.random2D(length_output_data, neurons_in_hidden_layer) * Utilities.xav_uni(length_output_data, neurons_in_hidden_layer)
 
     # weight initialization for he normal initialization method
     elif initialization_method == "he_nor":
-      '''input layer (dimension of weight matrix : hidden x input)'''
+      # input layer (dimension of weight matrix : hidden x input)
       weights[1] = Utilities.random2D(neurons_in_hidden_layer, length_input_data) * Utilities.he_nor(length_input_data)
-      '''hidden layers (dimension of weight matrix : hidden x hidden)'''
+      # hidden layers (dimension of weight matrix : hidden x hidden)
       for i in range(2, number_total_layers - 1):
         weights[i] = Utilities.random2D(neurons_in_hidden_layer, neurons_in_hidden_layer) * Utilities.he_nor(neurons_in_hidden_layer)
-      '''output layer (dimension of weight matrix : output x hidden)'''
+      # output layer (dimension of weight matrix : output x hidden)
       weights[number_total_layers - 1] = Utilities.random2D(length_output_data, neurons_in_hidden_layer) * Utilities.he_nor(neurons_in_hidden_layer)
 
     # weight initialization for he uniform initialization method
     elif initialization_method == "he_uni":
-      '''input layer (dimension of weight matrix : hidden x input)'''
+      # input layer (dimension of weight matrix : hidden x input)
       weights[1] = Utilities.random2D(neurons_in_hidden_layer, length_input_data) * Utilities.he_uni(length_input_data)
-      '''hidden layers (dimension of weight matrix : hidden x hidden)'''
+      # hidden layers (dimension of weight matrix : hidden x hidden)
       for i in range(2, number_total_layers - 1):
         weights[i] = Utilities.random2D(neurons_in_hidden_layer, neurons_in_hidden_layer) * Utilities.he_uni(neurons_in_hidden_layer)
-      '''output layer (dimension of weight matrix : output x hidden)'''
+      # output layer (dimension of weight matrix : output x hidden)
       weights[number_total_layers - 1] = Utilities.random2D(length_output_data, neurons_in_hidden_layer) * Utilities.he_uni(neurons_in_hidden_layer)
 
     return weights
@@ -60,42 +74,69 @@ def initializeWeight(initialization_method, number_total_layers, neurons_in_hidd
 
 '''function to initialize the bias'''
 def initializeBias(initialization_method, number_total_layers, neurons_in_hidden_layer, length_output_data):
+    '''
+      Parameters:
+        initialization_method : initailization method for the model
+        number_total_layers : total number of layers for the model
+        neurons_in_hidden_layer : number of neurons in the hidden layer of the model
+        length_input_data : length of input data
+        length_output_data : length of output data
+      Returns:
+        bias : a dictionary containing the biases across all the layers
+      Function:
+        initializes the biases according to the initlization method
+    '''
+
+    # a dictionary to represent the biases
     bias = {}
 
     # bias initialization for random initialization method
     if initialization_method == "random":
-      '''input and hidden layers'''
+      # input and hidden layers
       for i in range(1, number_total_layers - 1):
         bias[i] = Utilities.random1DFloat(neurons_in_hidden_layer)
-      '''output layer'''
+      # output layer
       bias[number_total_layers - 1] = Utilities.random1DFloat(length_output_data)
 
     else:
-      '''input and hidden layers'''
+      # input and hidden layers
       for i in range(1, number_total_layers - 1):
         bias[i] = Utilities.random1DFloat(neurons_in_hidden_layer)
-      '''output layer'''
+      # output layer
       bias[number_total_layers - 1] = Utilities.random1DFloat(length_output_data)
 
     return bias
 
 
 '''function to initialize the gradients'''
-def initializeGradients(num_total_layers, neurons_in_hidden_layer, input_size, output_size):  #input_size : x_train.shape[1]  output_size : y_train.shape[1]
+def initializeGradients(num_total_layers, neurons_in_hidden_layer, input_size, output_size):
+    '''
+      Parameters:
+        number_total_layers : total number of layers for the model
+        neurons_in_hidden_layer : number of neurons in the hidden layer of the model
+        input_size : length of input data
+        output_size : length of output data
+      Returns:
+        derivative_weights : a dictionary containing the initial weight change across all the layers
+        derivative_biases : a dictionary containing the initial bias change across all the layers
+      Function:
+        initializes the change of weights and biases
+    '''
+
     derivative_weights = {}
-    '''input layer'''
+    # input layer
     derivative_weights[1] = Utilities.create_zeros2D(neurons_in_hidden_layer, input_size)
-    '''hidden layer'''
+    # hidden layer
     for layer in range(2, num_total_layers - 1):
       derivative_weights[layer] = Utilities.create_zeros2D(neurons_in_hidden_layer, neurons_in_hidden_layer)
-    '''output layer'''
+    # output layer
     derivative_weights[num_total_layers - 1] = Utilities.create_zeros2D(output_size, neurons_in_hidden_layer)
 
     derivative_biases = {}
-    '''input and hidden layer'''
+    # input and hidden layer
     for layer in range(1, num_total_layers - 1):
       derivative_biases[layer] = Utilities.create_zeros1D(neurons_in_hidden_layer)
-    '''output layer'''
+    # output layer
     derivative_biases[num_total_layers - 1] = Utilities.create_zeros1D(output_size)
 
     return derivative_weights, derivative_biases
