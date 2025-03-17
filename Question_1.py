4eb4810c23b7365d69d2dd0a0860aaa83251aaf9
DEBASMITA2702 <69621397+DEBASMITA2702@users.noreply.github.com>
2025-03-17 19:46:10 +0530

    Added Comments

--- Question_1.py
+++ Question_1.py
@@ -6,17 +6,17 @@ from LoadDataset import loadAndSplitDataset
 
 wandb.login()
 
-'''loads dataset from fashion_mnist'''
+#loads dataset from fashion_mnist
 x_train, y_train, _, _, _, _ = loadAndSplitDataset(fashion_mnist)
 
-'''gives labels of the 10 output classes'''
+#gives labels of the 10 output classes
 output_classes = ["T-shirt/top", "Trouser", "Pullover", "Dress", "Coat", "Sandal", "Shirt", "Sneaker", "Bag", "Ankle boot"]
 
-'''creating subplots (2 rows, 5 columns) '''
+#creating subplots (2 rows, 5 columns)
 fig, axes = plt.subplots(2, 5, figsize=(20, 6))
 axes = axes.flatten()
 
-'''prepare a list to store the images'''
+#prepare a list to store the images
 wandb_images = []
 
 '''loop over each of the 10 classes'''
@@ -33,7 +33,7 @@ for current_class in range(10):
     single_image = wandb.Image(x_train[first_match_index], caption=[output_classes[current_class]])
     wandb_images.append(single_image)
 
-'''log all the images into wandb'''
+#log all the images into wandb
 wandb.init(project="Debasmita-DA6410-Assignment-1", name="Image for Question 1")
 wandb.log({"Question 1": wandb_images})
 wandb.finish()
