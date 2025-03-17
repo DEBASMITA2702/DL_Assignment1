d63b3cd3b73c563e7573b6e7d557650d3aa4b16e
DEBASMITA2702 <69621397+DEBASMITA2702@users.noreply.github.com>
2025-03-17 20:04:22 +0530

    Added Comments

--- train.py
+++ train.py
@@ -12,17 +12,24 @@ from ConfusionMatrix import createAndPlotConfusionMatrix
 
 if __name__ == '__main__':
     
+    # collect the arguments from command line
     argument_passed = collectArguments()
 
+    # create the hyperparameter dictionary
     hyperparam_dictionary = setValuesFromArgument(argument_passed)
 
+    # loads the dataset and splits it to training, validation and test sets based on the choice of the dataset
     x_train, y_train, x_val, y_val, x_test, y_test = loadAndSplitDataset(fashion_mnist)
     if (hyperparam_dictionary.get("dataset_name") == "mnist"):
         x_train, y_train, x_val, y_val, x_test, y_test = loadAndSplitDataset(mnist)
 
+    # login to wandb
     wandb.login()
+
+    # initialoze the project
     wandb.init(project = hyperparam_dictionary.get("project_name"), entity = hyperparam_dictionary.get("entity_name"))
 
+    # create the neural network object based on the values from the hyperparameter dictionary
     TrainPy_Network_model = NeuralNetwork(x_train, y_train, x_val, y_val, epochs = hyperparam_dictionary.get("epochs"), 
                                   num_hidden_layers = hyperparam_dictionary.get("hidden_layers"), 
                                   neurons_in_hidden_layer = hyperparam_dictionary.get("neurons_in_hidden"), 
@@ -33,6 +40,7 @@ if __name__ == '__main__':
                                   weight_decay = hyperparam_dictionary.get("weight_decay")
                                 )
     
+    # call the fitting function
     learned_weights, learned_biases = TrainPy_Network_model.fitModel(beta = hyperparam_dictionary.get("beta"), 
                                                              beta1 = hyperparam_dictionary.get("beta1"), 
                                                              beta2 = hyperparam_dictionary.get("beta2"),
@@ -43,7 +51,7 @@ if __name__ == '__main__':
                                                              momentum = hyperparam_dictionary.get("momentum")
                                                             )
     
-    '''run the model on the test data'''
+    # run the model on the test data and gather the predictions of the model
     x_test_flattened = flattenAndNormalizeInputImage(x_test)
     y_test_one_hot = oneHotEncodeOutput(y_test)
     
@@ -56,15 +64,16 @@ if __name__ == '__main__':
         predictions.append(predictedValues)
     
 
-    '''if test == 1'''
+    # if test == 1
     if hyperparam_dictionary.get("test") == 1:
         test_accuracy = trainingAccuracy(y_test_one_hot, np.array(predictions))
         
         '''print the test accuracy'''
         print("\nTest accuracy is : ", test_accuracy)
     
-    '''if confusion matrix == 1'''
+    # if confusion matrix == 1
     if hyperparam_dictionary.get("confusion_matrix"):
         createAndPlotConfusionMatrix(y_test_one_hot, predictions, dataset = hyperparam_dictionary.get("dataset_name"))
     
+    # finish wandb
     wandb.finish()
